213cc3ba5b5a612d5160a1d35cd2ac3b08d39607
hugode <hugo_dedaj@hotmail.com>
2018-07-05 20:51:33 +0200

    test buttons

--- app/src/main/java/com/example/hugo/chatroom/MainActivity.java
+++ app/src/main/java/com/example/hugo/chatroom/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.hugo.chatroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=(Button)findViewById(R.id.buttoni1);
+        btn2=(Button)findViewById(R.id.buttoni2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
