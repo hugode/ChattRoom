a8e33a2b7ad7629bb715153029082f34017243f8
hugode <hugo_dedaj@hotmail.com>
2018-07-11 18:00:01 +0200

    fix the gradlew

---
+++ app/src/main/java/com/ubt/ubtroom/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.ubt.ubtroom;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
