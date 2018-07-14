9318f1a9aa2fd147dd376a3001aa7b4719eaee52
hugode <hugo_dedaj@hotmail.com>
2018-07-14 04:28:34 +0200

    Change the App name

--- app/src/main/java/com/ubt/ubtroom/MainActivity.java
+++ app/src/main/java/com/ubt/ubtroom/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ubt.ubtroom;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
