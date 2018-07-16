cbf4a5b651d9f67c8510b96120d0310488992297
hugode <hugo_dedaj@hotmail.com>
2018-07-16 18:04:00 +0200

    Add the onCreate function

--- app/src/main/java/com/ubt/ubtroom/ProfileActivity.java
+++ app/src/main/java/com/ubt/ubtroom/ProfileActivity.java
@@ -1,4 +1,17 @@
 package com.ubt.ubtroom;
 
-public class ProfileActivity extends  {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+
+
+    }
 }
